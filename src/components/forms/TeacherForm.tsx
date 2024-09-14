0b9a85e9958a090509031d5e37a91ff2820c72b4
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-14 11:50:31 +0700

    Fix validation teacher and student form

--- src/components/forms/TeacherForm.tsx
+++ src/components/forms/TeacherForm.tsx
@@ -8,9 +8,9 @@ import Image from "next/image";
 const schema = z.object({
   username: z
     .string()
-    .min(3, { message: "Username must be at least 3 characters  long!" })
+    .min(3, { message: "Username must be at least 3 characters long!" })
     .max(20, { message: "Username must be at most 20 characters long!" }),
-  email: z.string().min(8, { message: "Invalid email address!" }),
+  email: z.string().email({ message: "Invalid email address!" }),
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long!" }),
